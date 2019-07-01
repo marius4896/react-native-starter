7ea747e15f61026a34b09ee132acde17a4810634
marius.raducanu <marius.raducanu@vodafone.com>
2019-07-01 19:28:18 +0300

    Added support for react-native-svg, react-native-svg-uri, react-native-vector-icons

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -3,11 +3,13 @@ import {
     View,
     Text
 } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 import styles from 'src/screens/styles/LoginScreenStyles.js';
 
 const LoginScreen = () => (
     <View style={styles.root}>
         <Text style={styles.text}>LoginScreen</Text>
+        <Icon name={"ios-alarm"} size={32} color={'orange'}  />
     </View>
 );
 
